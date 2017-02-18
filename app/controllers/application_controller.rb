d2e6d6c58eb70a4464a1ba6b39d47927fe5146bd
taemin <taeminwas@gmail.com>
2017-02-18 21:48:15 +0000

    Fix typo in hello application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render html: "Hello, everoyone."
+    render html: "Hello, everyone."
   end
 end
